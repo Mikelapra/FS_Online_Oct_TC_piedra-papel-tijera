cbd254f821fa91c7e916283ba4ae52f798bceff8
Mikel Apraiz <apraiz.mikel@gmail.com>
2023-11-11 18:13:57 +0100

    Entregable 3 finalizado

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const tijera = document.querySelector('.boton-jugada[data-jugada="tijera"]')
 let jugada = "";
 let jugadaOrdenador = "";
 let resultado = "";
+const contadorusuario = document.getElementById("contador-usuario");
+const contadorordenador = document.getElementById("contador-ordenador");
+let puntosusuarios = 0
+let puntosordenador = 0
 
 let posibilidades = ["piedra", "papel", "tijera"]
 
@@ -14,16 +18,25 @@ let comparar = function (indiceUsuario) {
     if (indiceUsuario == indiceOrdenador) {
         resultado = "Empate"
     } else if (indiceUsuario == indiceOrdenador -1 ) {
+        puntosordenador = puntosordenador + 1;
         resultado = "Gana ordenador"
     } else if (indiceUsuario == 2 && indiceOrdenador==0) {
+        puntosordenador = puntosordenador + 1;
         resultado = "Gana ordenador";
     } else {
+        puntosusuarios = puntosusuarios +1;
         resultado = "Ganas tú";
     }
 
     const resultados = document.getElementById("resultados")
     resultados.innerHTML=`<p>Tú ha elegido ${posibilidades[indiceUsuario]} y el ordenador ha elegido ${posibilidades[indiceOrdenador]} . ${resultado}</p>`
+contadorordenador.textContent =`Puntos de la máquina: ${puntosordenador}`
+contadorusuario.textContent =`tus puntos: ${puntosusuarios}`
 }
+
+
+
+
 piedra.addEventListener("click", function() {
     jugada = 0
     console.log(jugada)
